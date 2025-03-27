4175d0ac1a69ab7a216975e8dd5b3ee308d2b85c
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-03-26 23:50:23 -0300

    pre e pos incremento

--- operadores/n2_nc3_3.c
+++ operadores/n2_nc3_3.c
@@ -2,11 +2,20 @@
 
 int main()
 {
-    int numero1 = 1;
+    int numero1 = 1, resultado;
 
     printf("Antes do imcremento %d\n", numero1);
-    numero1++;
+    
     printf("Pós do imcremento %d\n", numero1);
     numero1--;
     printf("Pós do decremento %d\n", numero1);
+
+    resultado = numero1++;
+
+    printf("Apos Pós-incremento - Número  :%d - Resultado: %d\n", numero1, resultado);
+
+    resultado = ++numero1;
+
+    printf("Apos Pré-incremento - Número  :%d - Resultado: %d\n", numero1, resultado);
+
 }
