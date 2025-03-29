6bcc536f41082efb965948ccce735a3fd73f676e
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-03-29 18:20:20 -0300

    if else par ou impar

---
+++ if_else/n1r_nc3_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int numero1 = 10, numero2 = 10;
+
+    if (numero1 > numero2)
+    {
+        printf("O numero 1 é maior que o numero 2\n");
+    }
+    else if (numero1 == numero2)
+    {
+        printf("O numero 1 é igual ao numero 2\n");
+    }
+    else
+    {
+        printf("O numero 1 é menor que o numero 2\n");
+    }
+
+    return 0;
+}
